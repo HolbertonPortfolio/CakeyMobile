2571ac31136e0f03075ffcfab63773c61a650833
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-24 20:13:15 +0400

    Routing from onboarding to Splash

--- lib/Presentation/widgets/onboarding/onboarding_page_view.dart
+++ lib/Presentation/widgets/onboarding/onboarding_page_view.dart
@@ -1,3 +1,5 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:cakey_portfolio/Presentation/routes/app_router.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPageView extends StatefulWidget {
@@ -12,12 +14,14 @@ class OnboardingPageView extends StatefulWidget {
 class _OnboardingPageViewState extends State<OnboardingPageView> {
   final PageController _pageController = PageController();
   double currentPage = 0;
+  int activePage = 0;
 
   @override
   void initState() {
     _pageController.addListener(() {
       setState(() {
         currentPage = _pageController.page ?? 0;
+        activePage = currentPage.toInt();
       });
     });
     super.initState();
@@ -41,36 +45,71 @@ class _OnboardingPageViewState extends State<OnboardingPageView> {
         ),
         Positioned(
           bottom: 0,
-          left: 0,
-          right: 0,
+          left: 40,
           height: 100,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List<Widget>.generate(
-              widget.pages.length,
-              (index) => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: InkWell(
-                  onTap: () {
-                    _pageController.animateToPage(index,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List<Widget>.generate(
+                  widget.pages.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: InkWell(
+                      onTap: () {
+                        _pageController.animateToPage(index,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn);
+                      },
+                      child: AnimatedContainer(
                         duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn);
-                  },
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    height: 16,
-                    width: currentPage.round() == index ? 32 : 16,
-                    decoration: BoxDecoration(
-                      color: currentPage.round() == index
-                          ? Colors.amberAccent
-                          : Colors.grey,
-                      borderRadius: BorderRadius.circular(90),
+                        curve: Curves.easeInOut,
+                        height: 16,
+                        width: currentPage.round() == index ? 32 : 16,
+                        decoration: BoxDecoration(
+                          color: currentPage.round() == index
+                              ? Colors.amberAccent
+                              : Colors.grey,
+                          borderRadius: BorderRadius.circular(90),
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+              const SizedBox(
+                width: 90,
+              ),
+              InkWell(
+                onTap: () {
+                  if (activePage != widget.pages.length - 1)
+                  {
+                    activePage = currentPage.toInt() + 1;
+                    _pageController.animateToPage(currentPage.toInt() + 1,
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeIn);
+                  }
+                  else {
+                    activePage = currentPage.toInt();
+                    context.router.replace(const MainRoute());
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  width: 110,
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    activePage != widget.pages.length - 1
+                        ? 'Next'
+                        : 'Get Started',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
