d3ae1fa1a562afdd7934bab8aa72bbbb12d4836d
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-24 17:41:43 +0400

    New colors

--- lib/res/app_colors.dart
+++ lib/res/app_colors.dart
@@ -2,4 +2,7 @@ import 'dart:ui';
 
 class AppColors {
   static const Color mainTheme = Color(0xFFFFBFBF);
+  static const Color pink = Color(0xFFFFE5E5);
+  static const Color lightGreen = Color(0xFFF3FDE8);
+  static const Color green = Color(0xFFA8DF8E);
 }
