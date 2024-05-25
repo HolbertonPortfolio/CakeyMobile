c7d75b3ce0032f232894114f12e1b8de0ab750d7
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-26 00:56:07 +0400

    Simple BottomNavBar

--- lib/Presentation/screens/main_screen.dart
+++ lib/Presentation/screens/main_screen.dart
@@ -1,15 +1,92 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cakey_portfolio/res/app_colors.dart';
 import 'package:flutter/material.dart';
+
 @RoutePage()
-class MainScreen extends StatelessWidget {
+
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomeScreen(),
+    const SearchScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Main Screen'),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.pinkAccent,
+        unselectedItemColor: AppColors.mutedPink,
+        backgroundColor: AppColors.lightPink,
+        onTap: _onItemTapped,
+
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Screen', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Search Screen', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile Screen', style: TextStyle(fontSize: 24)),
     );
   }
 }
--- lib/res/app_colors.dart
+++ lib/res/app_colors.dart
@@ -1,8 +1,11 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color mainTheme = Color(0xFFFFBFBF);
   static const Color pink = Color(0xFFFFE5E5);
   static const Color lightGreen = Color(0xFFF3FDE8);
   static const Color green = Color(0xFFA8DF8E);
+  static const Color pinkAccent = Colors.pinkAccent;
+  static const Color lightPink = Color(0xFFFCE4EC);
+  static const Color mutedPink = Color(0xFFF48FB1);
 }
