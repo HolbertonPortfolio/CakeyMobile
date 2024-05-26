fcce02a1a28cab67b7bb0abd77281dbf34b89a49
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-27 02:22:17 +0400

    Request for single pastry

--- lib/Api/pastry_api_methods.dart
+++ lib/Api/pastry_api_methods.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:cakey_portfolio/Api/api_provider.dart';
 import 'package:cakey_portfolio/Data/pastry.dart';
+import 'package:cakey_portfolio/Data/pastry_detail.dart';
 import 'package:http/http.dart' as http;
 
 class PastryApiMethods {
@@ -15,4 +16,17 @@ class PastryApiMethods {
       throw Exception('Failed to load pastries');
     }
   }
+
+  Future<PastryDetail> getPastryById(int id) async {
+    final url = '${ApiProvider.baseUrl}/pastries/$id';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var pastryJson = jsonDecode(response.body);
+      return PastryDetail.fromJson(pastryJson);
+    } else {
+      throw Exception('Failed to load pastry with id $id');
+    }
+  }
+
 }
