54aa2ee527c7f6694433b64ac890ed331e9987e0
isaquliyev <isa.quluyev.wip@gmail.com>
2024-06-27 18:30:12 +0400

    Some ui fixes

--- lib/Presentation/screens/MainScreen/Pages/add_pastry/content/content_page_1.dart
+++ lib/Presentation/screens/MainScreen/Pages/add_pastry/content/content_page_1.dart
@@ -6,37 +6,41 @@ class ContentPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-          child: TextField(
-            decoration: InputDecoration(
-              constraints: BoxConstraints(maxHeight: 60.h),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text('Name', style: TextStyle(fontFamily: 'Haarith', fontSize: 24.sp, fontWeight: FontWeight.w500),),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+            child: TextField(
+              decoration: InputDecoration(
+                constraints: BoxConstraints(maxHeight: 60.h),
 
-              hintText: 'Name',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
+                hintText: 'Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
               ),
             ),
           ),
-        ),
-        24.verticalSpace,
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-          child: TextField(
-            maxLines: null,
-            minLines: 5,
-            decoration: InputDecoration(
-              constraints: BoxConstraints(),
-              hintText: 'Description',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
+          24.verticalSpace,
+          Text('Description', style: TextStyle(fontFamily: 'Haarith', fontSize: 24.sp, fontWeight: FontWeight.w500),),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+            child: TextField(
+              maxLines: null,
+              minLines: 10,
+              decoration: InputDecoration(
+                constraints: const BoxConstraints(),
+                hintText: 'Description',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
