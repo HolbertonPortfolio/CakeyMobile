92ac41215a7708a954503b3b054ead294f62f860
isaquliyev <isa.quluyev.wip@gmail.com>
2024-06-30 01:30:30 +0400

    Image picker implemented

--- lib/Presentation/screens/MainScreen/Pages/add_pastry/content/content_page_3.dart
+++ lib/Presentation/screens/MainScreen/Pages/add_pastry/content/content_page_3.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ContentPage3 extends StatelessWidget {
+class ContentPage3 extends StatefulWidget {
   const ContentPage3({super.key});
 
   @override
+  _ContentPage3State createState() => _ContentPage3State();
+}
+
+class _ContentPage3State extends State<ContentPage3> {
+  File? _image;
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('Content Page 3');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Load your image'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () => _showImagePickerOptions(context),
+              child: Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12.0),
+                  border: Border.all(color: Colors.grey[400]!),
+                ),
+                child: _image != null
+                    ? ClipRRect(
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: Image.file(
+                    _image!,
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                    : const Center(
+                  child: Text(
+                    'Tap to pick image',
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showImagePickerOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SafeArea(
+          child: Wrap(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Gallery'),
+                onTap: () {
+                  _pickImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Camera'),
+                onTap: () {
+                  _pickImage(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
